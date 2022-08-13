10762cf60d3eff22fc842d89312acea9387c308b
Frozen String <964413011@qq.com>
2022-08-13 17:27:11 +0800

    ➖  remove sika depend

--- src/fun/mod.rs
+++ src/fun/mod.rs
@@ -1,17 +1,14 @@
 use std::mem;
 
-use crate::event::listener::Listener;
-use crate::event::GroupMessageEvent;
-use crate::fun::drawmeme::drawmeme_listener;
+// use crate::fun::drawmeme::drawmeme_listener;
 use crate::fun::moli::moli_listener;
-use crate::get_app;
 
-mod drawmeme;
+// mod drawmeme;
 mod game;
 mod moli;
 
 pub fn handler() {
-    mem::forget(drawmeme_listener());
+    // mem::forget(drawmeme_listener());
 
     mem::forget(moli_listener());
 }
