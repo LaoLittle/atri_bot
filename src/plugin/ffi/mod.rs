93f606dc4aed3700c834eaf889ad378ac25e445d
LaoLittle <93422095+LaoLittle@users.noreply.github.com>
2022-08-27 22:38:22 +0800

    remove oncelock

--- src/plugin/ffi/mod.rs
+++ src/plugin/ffi/mod.rs
@@ -8,7 +8,6 @@ mod member;
 mod message;
 
 use atri_ffi::error::FFIResult;
-use std::sync::OnceLock;
 
 use crate::plugin::ffi::bot::{
     bot_find_friend, bot_find_group, bot_get_friends, bot_get_groups, bot_get_id, bot_get_list,
@@ -39,50 +38,50 @@ use atri_ffi::ffi::AtriVTable;
 use atri_ffi::future::FFIFuture;
 use atri_ffi::Managed;
 
-static PLUGIN_VTABLE: OnceLock<AtriVTable> = OnceLock::new();
+static PLUGIN_VTABLE: AtriVTable = AtriVTable {
+    plugin_manager_spawn,
+    plugin_manager_block_on,
+    new_listener,
+    event_intercept,
+    event_is_intercepted,
+    bot_get_id,
+    bot_get_nickname,
+    bot_get_list,
+    find_bot,
+    bot_find_group,
+    bot_find_friend,
+    bot_get_groups,
+    bot_get_friends,
+    group_message_event_get_group,
+    group_message_event_get_message,
+    group_message_event_get_sender,
+    group_get_id,
+    group_get_name,
+    group_get_bot,
+    group_get_members,
+    group_find_member,
+    group_get_named_member,
+    group_send_message,
+    group_upload_image,
+    group_quit,
+    group_change_name,
+    friend_message_event_get_friend,
+    friend_message_event_get_message,
+    friend_get_id,
+    friend_get_nickname,
+    friend_get_bot,
+    friend_send_message,
+    friend_upload_image,
+    named_member_get_id,
+    named_member_get_nickname,
+    named_member_get_card_name,
+    named_member_get_group,
+    named_member_change_card_name,
+    log_info,
+};
 
 pub fn get_plugin_vtable() -> *const AtriVTable {
-    PLUGIN_VTABLE.get_or_init(|| AtriVTable {
-        plugin_manager_spawn,
-        plugin_manager_block_on,
-        new_listener,
-        event_intercept,
-        event_is_intercepted,
-        bot_get_id,
-        bot_get_nickname,
-        bot_get_list,
-        find_bot,
-        bot_find_group,
-        bot_find_friend,
-        bot_get_groups,
-        bot_get_friends,
-        group_message_event_get_group,
-        group_message_event_get_message,
-        group_message_event_get_sender,
-        group_get_id,
-        group_get_name,
-        group_get_bot,
-        group_get_members,
-        group_find_member,
-        group_get_named_member,
-        group_send_message,
-        group_upload_image,
-        group_quit,
-        group_change_name,
-        friend_message_event_get_friend,
-        friend_message_event_get_message,
-        friend_get_id,
-        friend_get_nickname,
-        friend_get_bot,
-        friend_send_message,
-        friend_upload_image,
-        named_member_get_id,
-        named_member_get_nickname,
-        named_member_get_card_name,
-        named_member_get_group,
-        named_member_change_card_name,
-        log_info,
-    })
+    &PLUGIN_VTABLE
 }
 
 extern "C" fn plugin_manager_spawn(
