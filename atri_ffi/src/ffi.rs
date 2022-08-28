93bdd17f611cdff2190b3bc46615ef035ca59a77
LaoLittle <93422095+LaoLittle@users.noreply.github.com>
2022-08-28 11:06:08 +0800

    remove vtable

--- atri_ffi/src/ffi.rs
+++ atri_ffi/src/ffi.rs
@@ -1,75 +1,10 @@
-use crate::closure::FFIFn;
-use crate::error::FFIResult;
-use crate::future::FFIFuture;
-use crate::message::FFIMessageChain;
-use crate::{Managed, ManagedCloneable, RawVec, RustStr, RustString};
-
-use crate::contact::FFIMember;
-
-#[repr(C)]
-pub struct AtriVTable {
-    pub plugin_manager_spawn:
-        extern "C" fn(manager: *const (), FFIFuture<Managed>) -> FFIFuture<FFIResult<Managed>>,
-    pub plugin_manager_block_on: extern "C" fn(manager: *const (), FFIFuture<Managed>) -> Managed,
-
-    pub new_listener: extern "C" fn(FFIFn<FFIEvent, FFIFuture<bool>>) -> Managed,
-
-    pub event_intercept: extern "C" fn(intercepted: *const ()),
-    pub event_is_intercepted: extern "C" fn(intercepted: *const ()) -> bool,
-
-    pub bot_get_id: extern "C" fn(bot: *const ()) -> i64,
-    pub bot_get_nickname: extern "C" fn(bot: *const ()) -> RustStr,
-    pub bot_get_list: extern "C" fn() -> RawVec<ManagedCloneable>,
-    pub find_bot: extern "C" fn(id: i64) -> ManagedCloneable,
-    pub bot_find_group: extern "C" fn(bot: *const (), id: i64) -> ManagedCloneable,
-    pub bot_find_friend: extern "C" fn(bot: *const (), id: i64) -> ManagedCloneable,
-    pub bot_get_groups: extern "C" fn(bot: *const ()) -> RawVec<ManagedCloneable>,
-    pub bot_get_friends: extern "C" fn(bot: *const ()) -> RawVec<ManagedCloneable>,
-
-    pub group_message_event_get_group: extern "C" fn(event: *const ()) -> ManagedCloneable,
-    pub group_message_event_get_message: extern "C" fn(event: *const ()) -> FFIMessageChain,
-    pub group_message_event_get_sender: extern "C" fn(event: *const ()) -> FFIMember,
-
-    pub group_get_id: extern "C" fn(group: *const ()) -> i64,
-    pub group_get_name: extern "C" fn(group: *const ()) -> RustStr,
-    pub group_get_bot: extern "C" fn(group: *const ()) -> ManagedCloneable,
-    pub group_get_members: extern "C" fn(group: *const ()) -> FFIFuture<RawVec<ManagedCloneable>>,
-    pub group_find_member: extern "C" fn(group: *const (), id: i64) -> ManagedCloneable,
-    pub group_get_named_member:
-        extern "C" fn(group: *const (), id: i64) -> FFIFuture<ManagedCloneable>,
-    pub group_send_message:
-        extern "C" fn(group: *const (), chain: FFIMessageChain) -> FFIFuture<FFIResult<Managed>>,
-    pub group_upload_image:
-        extern "C" fn(group: *const (), data: RawVec<u8>) -> FFIFuture<FFIResult<Managed>>,
-    pub group_quit: extern "C" fn(group: *const ()) -> FFIFuture<bool>,
-    pub group_change_name:
-        extern "C" fn(group: *const (), name: RustString) -> FFIFuture<FFIResult<()>>,
-
-    pub friend_message_event_get_friend: extern "C" fn(event: *const ()) -> ManagedCloneable,
-    pub friend_message_event_get_message: extern "C" fn(event: *const ()) -> FFIMessageChain,
-    pub friend_get_id: extern "C" fn(friend: *const ()) -> i64,
-    pub friend_get_nickname: extern "C" fn(friend: *const ()) -> RustStr,
-    pub friend_get_bot: extern "C" fn(friend: *const ()) -> ManagedCloneable,
-    pub friend_send_message:
-        extern "C" fn(friend: *const (), chain: FFIMessageChain) -> FFIFuture<FFIResult<Managed>>,
-    pub friend_upload_image:
-        extern "C" fn(friend: *const (), img: RawVec<u8>) -> FFIFuture<FFIResult<Managed>>,
-
-    pub named_member_get_id: extern "C" fn(named: *const ()) -> i64,
-    pub named_member_get_nickname: extern "C" fn(named: *const ()) -> RustStr,
-    pub named_member_get_card_name: extern "C" fn(named: *const ()) -> RustStr,
-    pub named_member_get_group: extern "C" fn(named: *const ()) -> ManagedCloneable,
-    pub named_member_change_card_name:
-        extern "C" fn(named: *const (), card: RustString) -> FFIFuture<FFIResult<()>>,
-
-    pub log_info: extern "C" fn(handle: usize, manager: *const (), log: RustString),
-}
+use crate::ManagedCloneable;
 
 #[repr(C)]
 pub struct AtriManager {
     pub manager_ptr: *const (),
     pub handle: usize,
-    pub vtb: *const AtriVTable,
+    pub get_fun: extern "C" fn(sig: u16) -> *const (),
 }
 
 #[repr(C)]
