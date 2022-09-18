e4211d676c2d685b6dcbbaac133336554d4d07ef
LaoLittle <y1941803875@outlook.com>
2022-09-18 22:25:25 +0800

    update

--- atri_doc/docs/.vitepress/config.ts
+++ atri_doc/docs/.vitepress/config.ts
@@ -10,6 +10,12 @@ export default {
                     { text: '配置你的项目', link: '/primary/configure' },
                     { text: '运行你的插件', link: '/primary/run' }
                 ]
+            },
+            {
+                text: '其他',
+                items: [
+                    { text: '关于插件ABI', link: '/other/abi' }
+                ]
             }
         ]
     }
--- atri_doc/docs/other/abi.md
+++ atri_doc/docs/other/abi.md
@@ -1,6 +1,7 @@
-# 关于abi
+# 关于插件ABI
 
 ::: warning
-目前abi尚未稳定, 若有abi变动将会在release进行说明
-预计在v1.0稳定插件abi
+目前ABI尚未稳定, 若有ABI变动将会在Release进行说明
+
+预计在v1.0稳定插件ABI
 :::
