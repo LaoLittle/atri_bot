384a6bebb8bf804f717339c34a572d3ba1188fd7
LaoLittle <y1941803875@outlook.com>
2022-09-26 19:22:03 +0800

    update

--- atri_doc/docs/.vitepress/config.ts
+++ atri_doc/docs/.vitepress/config.ts
@@ -6,6 +6,7 @@ export default {
         sidebar: [
             {
                 text: '开始',
+                collapsible: true,
                 items: [
                     { text: '配置你的项目', link: '/primary/configure' },
                     { text: '运行你的插件', link: '/primary/run' }
@@ -13,6 +14,7 @@ export default {
             },
             {
                 text: '其他',
+                collapsible: true,
                 items: [
                     { text: '关于插件ABI', link: '/other/abi' }
                 ]
