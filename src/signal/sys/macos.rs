6eefb95a0ea5abd3eb74c6541fb8af004a185503
LaoLittle <y1941803875@outlook.com>
2022-12-15 22:31:06 +0800

    remove ::

--- src/signal/sys/macos.rs
+++ src/signal/sys/macos.rs
@@ -12,13 +12,13 @@ pub fn init_signal_hook() {
     };
 
     extern "C" {
-        fn sigemptyset(arg1: *mut SigsetT) -> ::std::os::raw::c_int;
+        fn sigemptyset(arg1: *mut SigsetT) -> std::os::raw::c_int;
 
         fn sigaction(
-            arg1: ::std::os::raw::c_int,
+            arg1: std::os::raw::c_int,
             arg2: *const Sigaction,
             arg3: *mut Sigaction,
-        ) -> ::std::os::raw::c_int;
+        ) -> std::os::raw::c_int;
     }
 
     unsafe {
@@ -35,16 +35,13 @@ pub const SIGBUS: std::ffi::c_int = 10;
 pub const SIGSEGV: std::ffi::c_int = 11;
 
 unsafe extern "C" fn handle(
-    sig: ::std::os::raw::c_int,
+    sig: std::os::raw::c_int,
     _info: *mut Siginfo,
-    _addr: *mut ::std::os::raw::c_void,
+    _addr: *mut std::os::raw::c_void,
 ) {
     fn dl_get_name(addr: *const std::ffi::c_void) -> *const std::ffi::c_char {
         extern "C" {
-            fn dladdr(
-                arg1: *const ::std::os::raw::c_void,
-                arg2: *mut DlInfo,
-            ) -> ::std::os::raw::c_int;
+            fn dladdr(arg1: *const std::os::raw::c_void, arg2: *mut DlInfo) -> std::os::raw::c_int;
         }
 
         let mut di = MaybeUninit::<DlInfo>::uninit();
@@ -85,34 +82,34 @@ pub type SigsetT = DarwinSigsetT;
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub struct Siginfo {
-    pub si_signo: ::std::os::raw::c_int,
-    pub si_errno: ::std::os::raw::c_int,
-    pub si_code: ::std::os::raw::c_int,
+    pub si_signo: std::os::raw::c_int,
+    pub si_errno: std::os::raw::c_int,
+    pub si_code: std::os::raw::c_int,
     pub si_pid: PidT,
     pub si_uid: UidT,
-    pub si_status: ::std::os::raw::c_int,
-    pub si_addr: *mut ::std::os::raw::c_void,
+    pub si_status: std::os::raw::c_int,
+    pub si_addr: *mut std::os::raw::c_void,
     pub si_value: Sigval,
-    pub si_band: ::std::os::raw::c_long,
-    pub __pad: [::std::os::raw::c_ulong; 7usize],
+    pub si_band: std::os::raw::c_long,
+    pub __pad: [std::os::raw::c_ulong; 7usize],
 }
 
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub union Sigval {
-    pub sival_int: ::std::os::raw::c_int,
-    pub sival_ptr: *mut ::std::os::raw::c_void,
+    pub sival_int: std::os::raw::c_int,
+    pub sival_ptr: *mut std::os::raw::c_void,
 }
 
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub union SigactionU {
-    pub __sa_handler: ::std::option::Option<unsafe extern "C" fn(arg1: ::std::os::raw::c_int)>,
-    pub __sa_sigaction: ::std::option::Option<
+    pub __sa_handler: Option<unsafe extern "C" fn(arg1: std::os::raw::c_int)>,
+    pub __sa_sigaction: Option<
         unsafe extern "C" fn(
-            arg1: ::std::os::raw::c_int,
+            arg1: std::os::raw::c_int,
             arg2: *mut Siginfo,
-            arg3: *mut ::std::os::raw::c_void,
+            arg3: *mut std::os::raw::c_void,
         ),
     >,
 }
@@ -122,14 +119,14 @@ pub union SigactionU {
 pub struct Sigaction {
     pub __sigaction_u: SigactionU,
     pub sa_mask: SigsetT,
-    pub sa_flags: ::std::os::raw::c_int,
+    pub sa_flags: std::os::raw::c_int,
 }
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct DlInfo {
-    pub dli_fname: *const ::std::os::raw::c_char,
-    pub dli_fbase: *mut ::std::os::raw::c_void,
-    pub dli_sname: *const ::std::os::raw::c_char,
-    pub dli_saddr: *mut ::std::os::raw::c_void,
+    pub dli_fname: *const std::os::raw::c_char,
+    pub dli_fbase: *mut std::os::raw::c_void,
+    pub dli_sname: *const std::os::raw::c_char,
+    pub dli_saddr: *mut std::os::raw::c_void,
 }
