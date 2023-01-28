8837ab9f34ef988b5265d7db55f1de73c766f385
LaoLittle <y1941803875@outlook.com>
2023-01-28 15:12:06 +0800

    update

--- src/signal/sys/unknown.rs
+++ src/signal/sys/unknown.rs
@@ -2,8 +2,8 @@ pub fn init_crash_handler() {
     ::tracing::warn!("当前系统暂未支持处理插件异常");
 }
 
-pub fn save_jmp() -> std::ffi::c_int {
-    0
+pub unsafe fn save_jmp() {
+
 }
 
 pub fn exception_jmp(status: std::ffi::c_int) -> ! {
